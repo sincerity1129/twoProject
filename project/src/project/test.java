62906cc91c4bd2f76f1c24bbb8a190a891f79ccf
smhrd <smhrd@221.156.60.69>
2021-03-19 22:46:14 +0900

    upload

--- project/src/project/test.java
+++ project/src/project/test.java
@@ -3,14 +3,7 @@ package project;
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("�׽�Ʈ��");
-		String test;
-		System.out.println("�谡�� ����");
-		System.out.println("�ֱٿ� �� �𸣰��� ��Ƴ׿�");
-		System.out.println("��â�� ����. ");
-		System.out.println("����?");
-		System.out.println("�׽�Ʈ");
-		System.out.println("�׽�Ʈ 2");
+		
 		System.out.println("정용현");
 	}
 
