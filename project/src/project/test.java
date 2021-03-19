d5bd390450a4b75e110f1ff473e4f57e05097787
sincerty1129 <sincerity1129@naver.com>
2021-03-20 00:11:10 +0900

    test

--- project/src/project/test.java
+++ project/src/project/test.java
@@ -3,8 +3,8 @@ package project;
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("aaa");
-		System.out.println("aaa");
+		System.out.println("한글");
+		System.out.println("한글");
 		System.out.println("abc");
 		System.out.println("aaaaa");
 	}
