c72cce9b0967c624b83552cc75334db5da1f4e86
smhrd <smhrd@220.71.167.73>
2021-03-19 14:55:19 +0900

    잘 모르겠어서 한번더 해봅니다

--- project/src/project/test.java
+++ project/src/project/test.java
@@ -5,7 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		String test;
 		System.out.println("김가연 성공");
-		System.out.println("최근영 ㅅㅅ");
+		System.out.println("최근영 잘 모르겠음 어렵네요");
 		System.out.println("박창윤 성공. ");
 		System.out.println("성공?");
 	}
