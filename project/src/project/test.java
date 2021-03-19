b75ae5b269bf29241adee9633e8da783c9d1e2df
sincerty1129 <sincerity1129@naver.com>
2021-03-20 00:08:49 +0900

    test

--- project/src/project/test.java
+++ project/src/project/test.java
@@ -3,8 +3,8 @@ package project;
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("변경중");
-		System.out.println("변경 중");
+		System.out.println("aaa");
+		System.out.println("aaa");
 		System.out.println("abc");
 		System.out.println("aaaaa");
 	}
