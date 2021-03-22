b6a598a918c1cb0010ddd83b903a30f40aee15df
kgyeonn <kgyeonn@gmail.com>
2021-03-22 13:01:42 +0900

    오류 해결

--- project/src/project/test2.java
+++ project/src/project/test2.java
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-package project;
-
-public class test2 {
-	public static void main(String[] args) {
-		
-
-	 String test;
-	 String pcy = "pcy";
-	 
-	 System.out.println(pcy);
-	}
-}
-=======
 package project;
 
 public class test2 {
@@ -27,4 +13,4 @@ public class test2 {
 	 System.out.println("±è°¡¿¬¼º°ø");
 	}
 }
->>>>>>> branch 'master' of https://github.com/sincerity1129/project.git
+
