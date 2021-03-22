ff284a1e695efcfff1fa0a7d8043aae9b5f3be55
cg-yeong <ds1506027@gmail.com>
2021-03-23 00:06:25 +0900

    xml test

--- project/src/project/geunyeong.java
+++ project/src/project/geunyeong.java
@@ -1,5 +1,39 @@
 package project;
 
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+
 public class geunyeong {
-	String yeong = "123";
+	public static void main(String[] args) throws IOException {
+        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1613000/AptListService1/getTotalAptList?serviceKey=R7lRK7HjdBh6uznMewpqOxlDXptvNRYPRL0psqVipMs%2FwHUrIoLI%2FdX2cQIZSBuw4AzwQUCtdpDvNx0sXtkbKg%3D%3D&pageNo=1&numOfRows=10"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=R7lRK7HjdBh6uznMewpqOxlDXptvNRYPRL0psqVipMs%2FwHUrIoLI%2FdX2cQIZSBuw4AzwQUCtdpDvNx0sXtkbKg%3D%3D"); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("bjdCode","UTF-8") + "=" + URLEncoder.encode("2638010100", "UTF-8")); /*법정동 코드*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지 번호*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        System.out.println("Response code: " + conn.getResponseCode());
+        BufferedReader rd;
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+        System.out.println(sb.toString());
+    }
+
 }
