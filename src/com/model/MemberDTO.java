59fe7315ca3bfe9b722e3362448412c7694c258e
cg-yeong <ds1506027@gmail.com>
2021-03-23 20:30:48 +0900

    dao test

---
+++ src/com/model/MemberDTO.java
@@ -0,0 +1,64 @@
+package com.model;
+
+public class MemberDTO {
+	
+	private String id;
+	private String pw;
+	private String name;
+	private String tel;
+	private String addr;
+	
+	
+	//·Î±×ÀÎ?
+	public MemberDTO(String id, String pw) {
+		super();
+		this.id = id;
+		this.pw = pw;
+	}
+
+	public MemberDTO(String id, String pw, String name, String tel, String addr) {
+		super();
+		this.id = id;
+		this.pw = pw;
+		this.name = name;
+		this.tel = tel;
+		this.addr = addr;
+	}
+	
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+	
+	
+	
+}
