35ba526940cf57dfa8b1ec982d547333f7e78fea
smhrd <smhrd@121.147.185.66>
2021-03-24 16:44:17 +0900

    sql

---
+++ WebContent/sql.sql
@@ -0,0 +1,11 @@
+create table budong_member(
+	id varchar2(10),
+	pw varchar2(15),
+	name varchar2(10)
+)
+
+select * from budong_member
+
+insert into budong_member values('smhrd','123','smhrd')
+
+select * from BUDONG_MEMBER
