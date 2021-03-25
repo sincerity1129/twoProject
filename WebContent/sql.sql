901c088ce057a60d44d2cc0986f5c655cf9193ee
smhrd <smhrd@121.147.185.66>
2021-03-25 13:44:48 +0900

    .

--- WebContent/sql.sql
+++ WebContent/sql.sql
@@ -1,3 +1,5 @@
+drop table budong_member
+
 create table budong_member(
 	id varchar2(10),
 	pw varchar2(15),
@@ -9,3 +11,36 @@ select * from budong_member
 insert into budong_member values('smhrd','123','smhrd')
 
 select * from BUDONG_MEMBER
+
+drop table junwall
+
+create table junwall(
+	junwall_num number,
+	dong_name varchar2(50),
+	build_year number,
+	deposit varchar2(10),
+	loyer varchar2(10),
+	apt_name varchar2(100),
+	year number,
+	month number,
+	day number,
+	apt_size number,
+	floor number
+)
+
+drop table maemae
+
+create table maemae(
+	maemae_num number,
+	dong_name varchar2(50),
+	price varchar2(10),
+	build_year number,
+	apt_name varchar2(100),
+	year number,
+	month number,
+	day number,
+	apt_size number,
+	floor number
+)
+
+
