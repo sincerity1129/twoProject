36e1b7d311c0cc95cce14e85ccedcea2d7c6f231
smhrd <smhrd@121.147.185.66>
2021-03-26 17:02:04 +0900

    수정

--- src/com/model/Main_filterDTO.java
+++ src/com/model/Main_filterDTO.java
@@ -1,5 +1,104 @@
 package com.model;
 
 public class Main_filterDTO {
+	
+	private int maemae_num;
+	private String dong;
+	private String price;
+	private int build_year;
+	private String apt_name;
+	private int year;
+	private int month;
+	private int day;
+	private int apt_size;
+	private int floor;
+	
+	
+	
+	
+	
+	
+	
+	public Main_filterDTO(String apt_name) {
+		super();
+		this.apt_name = apt_name;
+	}
+	
+	public Main_filterDTO(int maemae_num, String dong, String price, int build_year, String apt_name, int year,
+			int month, int day, int apt_size, int floor) {
+		super();
+		this.maemae_num = maemae_num;
+		this.dong = dong;
+		this.price = price;
+		this.build_year = build_year;
+		this.apt_name = apt_name;
+		this.year = year;
+		this.month = month;
+		this.day = day;
+		this.apt_size = apt_size;
+		this.floor = floor;
+	}
+	public int getApt_size() {
+		return apt_size;
+	}
+	public void setApt_size(int apt_size) {
+		this.apt_size = apt_size;
+	}
+	public int getMaemae_num() {
+		return maemae_num;
+	}
+	public void setMaemae_num(int maemae_num) {
+		this.maemae_num = maemae_num;
+	}
+	public String getDong() {
+		return dong;
+	}
+	public void setDong(String dong) {
+		this.dong = dong;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public int getBuild_year() {
+		return build_year;
+	}
+	public void setBuild_year(int build_year) {
+		this.build_year = build_year;
+	}
+	public String getApt_name() {
+		return apt_name;
+	}
+	public void setApt_name(String apt_name) {
+		this.apt_name = apt_name;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getMonth() {
+		return month;
+	}
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	public int getDay() {
+		return day;
+	}
+	public void setDay(int day) {
+		this.day = day;
+	}
+	public int getFloor() {
+		return floor;
+	}
+	public void setFloor(int floor) {
+		this.floor = floor;
+	}
+	
+	
 
 }
---
+++ src/com/service/SearchService.java
@@ -0,0 +1,42 @@
+package com.service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.controller.FrontCommand;
+import com.model.Main_filterDAO;
+import com.model.Main_filterDTO;
+
+public class SearchService implements FrontCommand {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		request.setCharacterEncoding("EUC-KR");
+		String search = request.getParameter("search");		
+		System.out.println(search);
+		
+		Main_filterDAO dao = new Main_filterDAO();
+		ArrayList<Main_filterDTO> searchDTO = dao.search(search);
+		
+		System.out.println(searchDTO);
+		System.out.println(search);
+		
+		
+		if(searchDTO != null) {
+			System.out.println("검색 성공");
+			
+		}
+		else {
+			System.out.println("검색 실패");
+		}
+		
+		response.sendRedirect("index.jsp");
+	}
+
+}
